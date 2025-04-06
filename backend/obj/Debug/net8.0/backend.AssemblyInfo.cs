705c0c425f5b2b19df7bbd89714b412ba4c9a442
ivansanmartin <ivansanmartin987@gmail.com>
2025-04-06 10:18:52 -0400

    feat: dockerfile and nginx.conf

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa612a3591d98a04d1ce2b61d2b56341c40648a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6dfd4a7835fb9a1e1bcdb8efe4feaa09b06761")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
