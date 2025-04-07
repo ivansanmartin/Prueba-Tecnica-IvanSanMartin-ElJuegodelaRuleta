4fe5b92474096e6f1c2dd79646bf503477227843
ivansanmartin <ivansanmartin987@gmail.com>
2025-04-06 23:12:30 -0400

    update: no profiel

--- frontend/src/components/profile/NoProfile.vue
+++ frontend/src/components/profile/NoProfile.vue
@@ -2,10 +2,15 @@
 	<div class="profile-info p-3">
 		<div class="card mt-4 d-flex flex-row">
 			<div class="card-body">
-				<div class="balance text-break">
-					<h6 class="mb-2 text-primary">Balance:</h6>
-					<i class="text-success fa-solid fa-dollar-sign"></i
-					><span>{{ balanceStore.balance }}</span>
+				<h6 class="card-subtitle mb-2 text-body-secondary fw-bold">
+					¿Quieres guardar tus ganancias?
+				</h6>
+
+                <p class="text-secondary text-center">Crea tú perfil y guarda las <br>ganacias de tus apuestas.</p>
+				<div class="balance text-break d-flex gap-1 align-items-center">
+					<h6 class="text-primary m-0">Balance:</h6>
+                    <span><i class="text-success fa-solid fa-dollar-sign"></i
+                        >{{ balanceStore.balance }}</span>
 				</div>
 			</div>
 		</div>
